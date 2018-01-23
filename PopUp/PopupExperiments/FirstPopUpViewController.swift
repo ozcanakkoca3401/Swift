235ecb8c19d2c0006b26ff0200fdf1d1e50cefcd
ozcanakkoca3401 <ozcanakkoca3401@gmail.com>
2018-01-23 16:19:13 +0300

    com

---
+++ PopUp/PopupExperiments/FirstPopUpViewController.swift
@@ -0,0 +1,64 @@
+//
+//  FirstPopUpViewController.swift
+//  PopupExperiments
+//
+//  Created by Erik Dungan on 6/12/16.
+//  Copyright © 2016 Callmeed. All rights reserved.
+//
+
+import UIKit
+
+class FirstPopUpViewController: UIViewController {
+    
+    @IBOutlet var panelView: UIView!
+    
+    // MARK: UIViewController overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Button action(s) 
+    @IBAction func didTapOk(_ sender: AnyObject) {
+        // TIP: If you're using a semi-transparent background (instead of clear)
+        //      this looks better if you set animated to FALSE
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: UI Configuration
+    func configureUI() {
+        // In order to have a transparent background and see through 
+        // to the presenting VC, these 2 settings are necessary on the view
+        // TIP: you can also use a color with alpha such as UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.5)
+        view.backgroundColor = UIColor.clear
+        view.isOpaque = false
+        // You can also perform optional decorations such as round corners & shadows
+        setRoundCorners()
+        setDropShadow()
+        setBorder()
+    }
+    
+    func setRoundCorners() {
+        panelView.layer.cornerRadius = 9
+    }
+    
+    func setBorder() {
+        panelView.layer.borderWidth = 3.0
+        panelView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func setDropShadow() {
+        panelView.layer.shadowColor = UIColor.black.cgColor
+        panelView.layer.shadowOpacity = 0.5
+        panelView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        panelView.layer.shadowRadius = 1.0
+
+    }
+    
+}
---
+++ PopUp/PopupExperiments/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  PopupExperiments
+//
+//  Created by Erik Dungan on 6/12/16.
+//  Copyright © 2016 Callmeed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    // MARK: UIViewController overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Button actions & handlers
+    @IBAction func didTapShowButton(_ sender: AnyObject) {
+        showPopUp()
+    }
+    
+    // MARK: Pup-up
+    func showPopUp() {
+        self.performSegue(withIdentifier: "FirstPopUpSegue", sender: self)
+    }
+}
+
